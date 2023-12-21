e721b75905d31b2eaab0104a607f1e7e11328ac0
Riccardo Talami <riccardo@MacBook-Pro-di-Riccardo.local>
2023-12-21 10:37:07 +0800

    update

--- src/pages/Publications.js
+++ src/pages/Publications.js
@@ -13,8 +13,9 @@ function Publications() {
                 <div className="individual_publication_section">
                     <h3>Journals</h3>
                     <ul>
+                        <li>Talami, R., Dawoodjee, I. and Ghahramani, A., 2023. <a href="https://doi.org/10.3390/buildings13122998" target="_blank">Quantifying Energy Savings from Optimal Selection of HVAC Temperature Setpoints and Setbacks across Diverse Occupancy Rates and Patterns</a>. <i>Buildings, 13(12)</i>, 2998</li>
                         <li>Talami, R., Dawoodjee, I. and Ghahramani, A., 2023. <a href="https://doi.org/10.1016/j.enbenv.2023.07.001" target="_blank">Demystifying energy savings from dynamic temperature setpoints under weather and occupancy variability</a>. <i>Energy and Built Environment</i></li>
-                        <li>Talami, R. and Jakubiec, J.A., 2020. <a href="https://www.sciencedirect.com/science/article/abs/pii/S0378778819333614" target="_blank">Early-design sensitivity of radiant cooled office buildings in the tropics for building performance</a>. <i>Energy and Buildings, 223</i>, 110177</li>
+                        <li>Talami, R. and Jakubiec, J.A., 2020. <a href="https://doi.org/10.1016/j.enbuild.2020.110177" target="_blank">Early-design sensitivity of radiant cooled office buildings in the tropics for building performance</a>. <i>Energy and Buildings, 223</i>, 110177</li>
                     </ul>
                 </div>
                 <div className="individual_publication_section">
