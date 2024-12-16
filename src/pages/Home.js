2173b16088a3f6fb6391ac52d986ffadd3783e34
unknown <user@SEC-L10975.sec.local>
2024-12-16 15:30:32 +0800

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -29,11 +29,11 @@ function Home() {
             <div className="Home_Sections">
                 <div className="individual_home_section">
                     <h3>Research Fellow at National University of Singapore</h3>
-                    <p className='home_sentences'><b>Riccardo Talami, PhD </b> is a Research Fellow at the National University of Singapore (NUS) – Department of Architecture, and the Singapore-ETH Centre.</p>
+                    <p className='home_sentences'><b>Riccardo Talami, PhD </b> is a Research Fellow at the <b> National University of Singapore (NUS) </b> – Department of Architecture, and the <b> Singapore-ETH Centre.</b> </p>
 
-                    <p className='home_sentences'>Riccardo specializes in developing cutting-edge computational methods, workflows, and tools that transform performance-based building and urban design. His work empowers building and urban stakeholders to shape sustainable, comfortable, smart, and efficient built environments.</p>
+                    <p className='home_sentences'>Riccardo specializes in developing cutting-edge computational methods, workflows, and tools that transform <b> performance-based building and urban design.</b> His work empowers building and urban stakeholders to shape sustainable, comfortable, smart, and efficient built environments.</p>
 
-                    <p className='home_sentences'>Riccardo is a strong advocate for data-driven building and urban design and operation, leveraging Computational Optimization and Artifical Intelligence to tackle the challenges of complex built environments.</p>
+                    <p className='home_sentences'>Riccardo is a strong advocate for <b> data-driven </b> building and urban design and operation, leveraging Computational Optimization and Artifical Intelligence to tackle the challenges of complex built environments.</p>
                    
 
                     {/* <h3 className='interests'>Interests</h3>
