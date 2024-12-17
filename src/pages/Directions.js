238bf3f53b0fe9c99832369901acfd0709820e51
unknown <user@SEC-L10975.sec.local>
2024-12-17 18:24:54 +0800

    update

--- src/pages/Directions.js
+++ src/pages/Directions.js
@@ -14,7 +14,7 @@ function Directions() {
             </div>
             <div className="Directions_Sections">
                 <p className='text'>I am reimagining the design process by breaking traditional silos between disciplines and fostering <b>true interdisciplinary collaboration</b>. By leveraging <b>data-and-performance-driven approaches</b> as the driving force of design, I am to transform how design alternatives are generated, explored, evaluated and selected.</p>
-                <p className='text'>Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here</p>
+                <p className='text'>My reseearch aims to empower built environment stakeholders and decision-makers—architects, engineers, urban planners and government agencies—with cutting-edge computational methods, workflows, and tools.</p>
                 <p className='text'>Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here</p>
                 <p className='text'>Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here</p>
                 <img src={Image1} />
