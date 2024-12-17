e981b1ce96a96c14e65619594084d972fda31dbd
unknown <user@SEC-L10975.sec.local>
2024-12-17 19:26:48 +0800

    update

--- src/pages/Directions.js
+++ src/pages/Directions.js
@@ -2,8 +2,8 @@ import React from 'react'
 import NavBar from '../components/NavBar'
 import './Directions.css'
 import Design_process from './../components/Design_process.png'
+import Design_innovative from './../components/Design_innovative.png'
 
-import PlaceHolderImage from './../components/PlaceHolderImage.jpg'
 
 function Directions() {
     return (
@@ -13,14 +13,15 @@ function Directions() {
                 <h1>Directions</h1>
             </div>
             <div className="Directions_Sections">
-                <p className='text'>I am reimagining the design process by breaking traditional silos between disciplines and fostering <b>true interdisciplinary collaboration</b>. By leveraging <b>data-and-performance-driven approaches</b> as the driving force of design, I aim to transform how design alternatives are generated, explored, evaluated and selected.</p>
-                <p className='text'>My reseearch aims to empower built environment stakeholders and decision-makers—architects, engineers, urban planners and government agencies—with cross-scale computational methods, workflows, and tools. These aim to enable:</p>
+                <p className='text'>I am reimagining the design process by breaking traditional silos between disciplines and fostering <b>true interdisciplinary collaboration</b>. My research aims to empower built environment stakeholders and decision-makers—architects, engineers, urban planners, facilities managers, and government agencies—with <b>cross-scale computational methods, workflows, and tools</b>.</p>
+                <img src={Design_process} />
+                <p className='text'>At the core of my research, I am envisioning a <b>dynamic, adaptive, and iterative design process</b>. Through this process, I aim to transform how design alternatives are generated, explored, evaluated and selected:</p>
                 <p className='text'>1. Automated design generation.</p>
                 <p className='text'>2. Rapid and smart design exploration.</p>
                 <p className='text'>3. Data-and-performance-driven design evaluation.</p>
                 <p className='text'>4. Informed decision-making design selection.</p>
-                <img src={Design_process} />
-                <p className='text'>Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here</p>
+                <img src={Design_innovative} />
+                <p className='text'>This enables built environment stakeholders to navigate complex decisions, bridging the gap between design ideas and actionable design solutions.</p>
             </div>
         </div>
     )
