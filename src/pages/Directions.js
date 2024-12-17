0de98f849bb77c4610bcb6bcaa1fdeee80f727a9
unknown <user@SEC-L10975.sec.local>
2024-12-17 18:28:29 +0800

    update

--- src/pages/Directions.js
+++ src/pages/Directions.js
@@ -14,9 +14,11 @@ function Directions() {
             </div>
             <div className="Directions_Sections">
                 <p className='text'>I am reimagining the design process by breaking traditional silos between disciplines and fostering <b>true interdisciplinary collaboration</b>. By leveraging <b>data-and-performance-driven approaches</b> as the driving force of design, I am to transform how design alternatives are generated, explored, evaluated and selected.</p>
-                <p className='text'>My reseearch aims to empower built environment stakeholders and decision-makers—architects, engineers, urban planners and government agencies—with cutting-edge computational methods, workflows, and tools.</p>
-                <p className='text'>Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here</p>
-                <p className='text'>Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here</p>
+                <p className='text'>My reseearch aims to empower built environment stakeholders and decision-makers—architects, engineers, urban planners and government agencies—with cross-scale computational methods, workflows, and tools. These aim to enable:</p>
+                <p className='text'>1. Automated design generation.</p>
+                <p className='text'>2. Rapid and smart design exploration.</p>
+                <p className='text'>3. Data-and-performance-driven design evaluation.</p>
+                <p className='text'>4. Informed decision-making design selection.</p>
                 <img src={Image1} />
                 <p className='text'>Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here Some Text here</p>
             </div>
