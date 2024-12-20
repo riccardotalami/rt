aecd77f56c437c64f8e96e1ef34b142a3c9d2837
unknown <user@SEC-L10975.sec.local>
2024-12-20 18:16:14 +0800

    update

--- src/pages/Active.js
+++ src/pages/Active.js
@@ -2,6 +2,8 @@ import React from 'react'
 import NavBar from '../components/NavBar'
 import './Active.css'
 import Concordia_University from './../components/Concordia_University.png'
+import SEC_logo from './../components/SEC_logo.png'
+import NUS_logo from './../components/NUS_logo.jpg'
 
 function Active() {
     return (
@@ -16,7 +18,7 @@ function Active() {
                         <ul>
                             <li>
                                 <p><b>Concordia University in Montreal (Canada)</b></p>
-                                <p>Project: Decomposition approaches in building design</p>
+                                <p>Project: Problem decomposition approaches in Building Design</p>
                                 <p>Contact: Nima Bonyadi, PhD Research Assistant</p>
                             </li>
                         </ul>
@@ -29,42 +31,42 @@ function Active() {
                     <div>
                         <ul>
                             <li>
-                                <p><b>Concordia University in Montreal (Canada)</b></p>
-                                <p>Project: Decomposition approaches in building design</p>
-                                <p>Contact: Nima Bonyadi, PhD Research Assistant</p>
+                                <p><b>Singapore-ETH Centre (Singapore)</b></p>
+                                <p>Project: AI-driven frameworks for Building Performance Design</p>
+                                <p>Contact: Dr. Alberto Costa, Senior Researcher</p>
                             </li>
                         </ul>
                     </div>
                     <div>
-                        <img src={Concordia_University} />
+                        <img src={SEC_logo} />
                     </div>
                 </div>
                 <div className="individual_active_section">
                     <div>
                         <ul>
                             <li>
-                                <p><b>Concordia University in Montreal (Canada)</b></p>
-                                <p>Project: Decomposition approaches in building design</p>
-                                <p>Contact: Nima Bonyadi, PhD Research Assistant</p>
+                                <p><b>National University of Singapore (Singapore)</b></p>
+                                <p>Project: The role of Archetypes in Urban Building Energy Modelling</p>
+                                <p>Contact: Wanyu Pei, PhD student</p>
                             </li>
                         </ul>
                     </div>
                     <div>
-                        <img src={Concordia_University} />
+                        <img src={NUS_logo} />
                     </div>
                 </div>
                 <div className="individual_active_section">
                     <div>
                         <ul>
                             <li>
-                                <p><b>Concordia University in Montreal (Canada)</b></p>
-                                <p>Project: Decomposition approaches in building design</p>
-                                <p>Contact: Nima Bonyadi, PhD Research Assistant</p>
+                                <p><b>Singapore-ETH Centre (Singapore)</b></p>
+                                <p>Project: The impact of thermal inertia in mitigating energy disruptions</p>
+                                <p>Contact: Dr. Andrea Bartolini, Postdoctoral Researcher</p>
                             </li>
                         </ul>
                     </div>
                     <div>
-                        <img src={Concordia_University} />
+                        <img src={SEC_logo} />
                     </div>
                 </div>
             </div>
